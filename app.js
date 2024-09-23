d7bd2aaa551c5836ceb6f0e6e54920481d4363de
Brandon Rafael Lovelyno <brandonrafael80@gmail.com>
2024-09-23 11:07:22 +0700

    feat:cors policy and morgan

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
 const app = express();
 app.use(express.json());
 
+app.use(morgan("dev"));
 
-app.get('/test', (req, res) => {
-    res.status(200).json({
-        message: 'Success'
-    });
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.get("/test", (req, res) => {
+  res.status(200).json({
+    message: "Success",
+  });
 });
 
 app.use("/user", require("./routes/userRoutes"));
 app.use("/kopi", require("./routes/kopiRoutes"));
 app.use("/login", require("./routes/loginRoutes"));
-
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,27 +1,25 @@
-const dotenv = require('dotenv');
-const path = require('path');
-const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+const path = require("path");
+const mongoose = require("mongoose");
 
-dotenv.config(
-    {path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`)}
-);
+dotenv.config({ path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`) });
 
-const app = require('./app');
-
+const app = require("./app");
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.info(`Server running on port ${PORT}`);
+  console.info(`Server running on port ${PORT}`);
 });
 
 if (!process.env.MONGO_URI) {
-    throw Error("Database connection string not found");
-  }
-  mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-      console.log("Succesfully connected to MongoDB");
-    }).catch((err) => {
-      console.log("Failed to connect to MongoDB");
-      console.log(err);
-    });
-  
+  throw Error("Database connection string not found");
+}
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Succesfully connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Failed to connect to MongoDB");
+    console.log(err);
+  });
