e76dbb4b98f40125c79c1a674ed91c77181b3d12
gabriel <gabrielsyailendraf@mail.ugm.ac.id>
2024-09-24 11:38:59 +0700

    add cookieparser

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const cookieParser = require('cookie-parser');
 
 const app = express();
 app.use(express.json());
-
+app.use(cookieParser());
 app.use(morgan("dev"));
 
 app.use(
